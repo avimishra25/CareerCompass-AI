4c43b89139a26c01a148e1c7138d619dcefe2371
Avi Mishra <aviam2425@gmail.com>
2026-04-17 17:21:03 +0530

    api.js

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import API_BASE_URL from './api';
+import API_BASE_URL from '../api';
 
 export default function History({ onNavigate }) {
   const [history,   setHistory]   = useState([]);
